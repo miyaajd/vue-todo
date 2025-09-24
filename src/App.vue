c942c5397b81e23d984bdea3720b2cc9b04a8082
miyaajd <ehdal1032@gmail.com>
2025-09-25 00:10:27 +0900

    create

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup></script>
+
+<template>
+  <div class="wrap">
+    <h1>◎ To Do List ◉</h1>
+  </div>
+</template>
+
+<style scoped>
+.wrap{
+  width: 100%;
+  height: 100vh;
+}
+h1 {
+  width: 100%;
+  text-align: center;
+  padding: 50px;
+  color: #333;
+}
+</style>
